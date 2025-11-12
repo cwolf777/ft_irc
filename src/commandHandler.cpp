290aa213ef0070dfd121f2925dfda29acfe348af
Christian Wolf <cwolf@3-F-10.42heilbronn.de>
2025-11-12 16:24:44 +0100

    first structure of cmd parsing

--- src/commandHandler.cpp
+++ src/commandHandler.cpp
@@ -1,8 +1,67 @@
+#include <string>
+#include <sstream>
+#include <vector>
 
+//NICK Chris
+void commandHandler(std::string msg) //+ welcher user/client
+{
+    std::stringstream ss(msg);
+    std::vector<std::string> arg;
+    while (ss >> msg)
+    {
+        arg.push_back(msg);
+    }
 
-//commandHandler(Client &client, const std::string message)
-//parts = split
-//cmd = parts[0]
+    if (arg[0] == "PASS")
+    {
 
-//if(cmd == "join")
-//....
+    }
+    else if (arg[0] == "NICK")
+    {
+            //handlenick
+    }
+    else if (arg[0] == "USER")
+    {
+
+    }
+    else if (arg[0] == "OPER")
+    {
+
+    }
+    else if (arg[0] == "MODE")
+    {
+
+    }
+    else if (arg[0] == "NICK")
+    {
+
+    }
+    else if (arg[0] == "QUIT")
+    {
+
+    }
+    else if (arg[0] == "JOIN")
+    {
+
+    }
+    else if (arg[0] == "TOPIC")
+    {
+
+    }
+    else if (arg[0] == "KICK")
+    {
+
+    }
+    else if (arg[0] == "PRIVMSG")
+    {
+
+    }
+    else if (arg[0] == "NOTICE")
+    {
+
+    }
+    else
+    {
+        //philipp sagen, dass falscher cmd 
+    }
+}
