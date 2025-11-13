2b20fc71a236511717539f81ad5251bba7d9ead5
Philipp Hofmann <phhofman@3-F-11.42heilbronn.de>
2025-11-13 11:03:46 +0100

    removed 42 headers in cmd branch

--- src/commandFunctions.cpp
+++ src/commandFunctions.cpp
@@ -1,19 +1,9 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   commandFunctions.cpp                               :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: cwolf <cwolf@student.42.fr>                +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/11/13 10:52:21 by cwolf             #+#    #+#             */
-/*   Updated: 2025/11/13 10:57:01 by cwolf            ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+
 
 void handlePass(Client &client, const std::vector<std::string> &args)
 {
-    //pruefen ob registriert 
-    //pruefen ob arg == 2
-    //preufen ob arg[1] == passwort von user
-    //falls ja, speichern dass pw akzeptiert wurde
+    // pruefen ob registriert
+    // pruefen ob arg == 2
+    // preufen ob arg[1] == passwort von user
+    // falls ja, speichern dass pw akzeptiert wurde
 }
