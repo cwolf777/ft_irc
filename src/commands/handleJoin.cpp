d597ddb6cc68d709eac96e4fd533412dbcdade15
Philipp Hofmann <phhofman@3-E-30.42heilbronn.de>
2026-01-29 12:44:56 +0100

    added channel name check in handleJoin and made function cleaner

--- src/commands/handleJoin.cpp
+++ src/commands/handleJoin.cpp
@@ -1,102 +1,112 @@
 #include "Server.hpp"
 #include "Channel.hpp"
+#include <algorithm>
 
-// TODO: cleaner machen
+static bool isValidChannelName(const std::string &name)
+{
+    if (name.empty() || name.size() > 50)
+        return false;
+
+    if (name[0] != '#' || name[0] != '&')
+        return false;
+    for (size_t i = 0; i < name.length(); ++i)
+    {
+        if (name[i] == ' ' || name[i] == ',' || name[i] == '\a')
+            return false;
+    }
+    return true;
+}
+
+static std::vector<std::string> split(const std::string &s, char delimiter)
+{
+    std::vector<std::string> tokens;
+    std::string token;
+    std::istringstream tokenStream(s);
+    while (std::getline(tokenStream, token, delimiter))
+    {
+        if (!token.empty())
+            tokens.push_back(token);
+    }
+    return tokens;
+}
+
+static void processJoin(Client &client, Server &server, const std::string &name, const std::string &key)
+{
+    ServerState &state = server.getServerState();
+
+    if (!isValidChannelName(name))
+    {
+        server.sendMsg(client, ":" + server.getServerName() + " 476 " + client.getNickname() + " " + name + " :Bad Channel Mask\r\n");
+        return;
+    }
+
+    Channel *channel = state.getChannel(name);
+    if (!channel)
+    {
+        Channel newChannel(name);
+        newChannel.addOperator(&client);
+        state.addChannel(newChannel);
+        channel = state.getChannel(name);
+    }
+
+    if (channel->isInviteOnly() && !channel->isInvited(client))
+    {
+        server.sendMsg(client, ":" + server.getServerName() + " 473 " + client.getNickname() + " " + name + " :Cannot join channel (+i)\r\n");
+        return;
+    }
+    if (client.getChannels().size() >= state.getChannelLimit())
+    {
+        server.sendMsg(client, ":" + server.getServerName() + " 405 " + client.getNickname() + " " + name + " :You have joined too many channels\r\n");
+        return;
+    }
+    if (channel->isUserLimitSet() && channel->getMembers().size() >= channel->getUserLimit())
+    {
+        server.sendMsg(client, ":" + server.getServerName() + " 471 " + client.getNickname() + " " + name + " :Cannot join channel (+l)\r\n");
+        return;
+    }
+    if (channel->isPasswordSet() && channel->getPassword() != key)
+    {
+        server.sendMsg(client, ":" + server.getServerName() + " 475 " + client.getNickname() + " " + name + " :Cannot join channel (+k)\r\n");
+        return;
+    }
+
+    // add client
+    channel->addMember(&client);
+    client.joinChannel(channel);
+
+    std::cout << *channel << std::endl;
+    // send to everyone in channel a message
+    std::string joinMsg = ":" + client.getPrefix() + " JOIN :" + name + "\r\n";
+    server.broadcastToChannel(client, *channel, joinMsg);
+
+    // send client the topic of the channel he joined
+    IrcMsg topic("TOPIC " + name + "\r\n");
+    server.handleRequest(client, topic);
+    IrcMsg names("NAMES " + name + "\r\n");
+    server.handleRequest(client, names);
+}
+
 void handleJoin(Client &client, Server &server, const IrcMsg &msg)
 {
     // ERR_NEEDMOREPARAMS Join command needs channel name as parameter : JOIN #foobar
     if (msg.get_params().empty())
     {
-        std::string reply = ":" + server.getServerName() + " 461 " + client.getNickname() + "JOIN :Not enough parameters\r\n";
-        server.sendMsg(client, reply);
+        server.sendMsg(client, ":" + server.getServerName() + " 461 " + client.getNickname() + "JOIN :Not enough parameters\r\n");
         return;
     }
 
-    ServerState &state = server.getServerState();
-    std::vector<std::string> joinList, passwordList;
-    std::string segment;
-
-    // splitting the channels from params
-    std::stringstream ss(msg.get_params()[0]);
-    while (std::getline(ss, segment, ','))
-    {
-        if (!segment.empty())
-            joinList.push_back(segment);
-    }
-    segment.clear();
-
-    // splitting the keys from params
+    // splitting the channels and keys from params
+    std::vector<std::string> channels = split(msg.get_params()[0], ',');
+    std::vector<std::string> keys;
     if (msg.get_params().size() > 1)
+        keys = split(msg.get_params()[1], ',');
+
+    // loop through channels to join each channel
+    for (size_t i = 0; i < channels.size(); i++)
     {
-        std::stringstream ssPass(msg.get_params()[1]);
-        while (std::getline(ssPass, segment, ','))
-        {
-            passwordList.push_back(segment);
-        }
-    }
-    // loop through joinList to join each channel
-    for (size_t i = 0; i < joinList.size(); i++)
-    {
-        std::string currChannelName = joinList[i];
-        std::string currPass = (i < passwordList.size()) ? passwordList[i] : "";
-
-        // 403 ERR_NOSUCHCHANNEL Channel currChannelName must start with # or &
-        if (currChannelName.empty() || (currChannelName[0] != '&' && currChannelName[0] != '#'))
-        {
-            std::string reply = ":" + server.getServerName() + " 403 " + client.getNickname() + " " + currChannelName + " :No such channel\r\n";
-            server.sendMsg(client, reply);
-            continue;
-        }
-
-        //  if channel dont exist => create channel
-        Channel *currChannel = state.getChannel(currChannelName);
-        if (!currChannel)
-        {
-            Channel newChannel(currChannelName);
-            newChannel.addOperator(&client); // first user becomes admin
-            state.addChannel(newChannel);
-            currChannel = state.getChannel(currChannelName);
-        }
-
-        // 473 ERR_INVITEONLYCHAN client has to be in invited vector
-        if (currChannel->isInviteOnly() && !currChannel->isInvited(client))
-        {
-            server.sendMsg(client, ":" + server.getServerName() + " 473 " + client.getNickname() + " " + currChannelName + " ::Cannot join channel (+i)\r\n");
-            return;
-        }
-        // 405 ERR_TOOMANYCHANNELS  check server Channellimit
-        if (client.getChannels().size() >= state.getChannelLimit())
-        {
-            server.sendMsg(client, ":" + server.getServerName() + " 405 " + client.getNickname() + " " + currChannelName + " :You have joined too many channels\r\n");
-            return;
-        }
-        // 471 ERR_CHANNELISFULL check if channel is full
-        if (currChannel->isUserLimitSet() && currChannel->getMembers().size() >= currChannel->getUserLimit())
-        {
-            std::string reply = ":" + server.getServerName() + " 471 " + client.getNickname() + " " + currChannelName + " :Cannot join channel (+l)\r\n";
-            server.sendMsg(client, reply);
-            continue;
-        }
-        // 475 ERR_BADCHANNELKEY check if password is set and correct
-        if (currChannel->isPasswordSet() && currChannel->getPassword() != currPass)
-        {
-            std::string reply = ":" + server.getServerName() + " 475 " + client.getNickname() + " " + currChannelName + " :Cannot join channel (+k)\r\n";
-            server.sendMsg(client, reply);
-            continue;
-        }
-        // add client
-        currChannel->addMember(&client);
-        client.joinChannel(currChannel);
-
-        std::cout << *currChannel << std::endl;
-        // send to everyone in channel a message
-        std::string joinMsg = ":" + client.getPrefix() + " JOIN :" + currChannelName + "\r\n";
-        server.broadcastToChannel(client, *currChannel, joinMsg);
-
-        // send client the topic of the channel he joined
-        IrcMsg topic("TOPIC " + currChannelName + "\r\n");
-        server.handleRequest(client, topic);
-        IrcMsg names("NAMES " + currChannelName + "\r\n");
-        server.handleRequest(client, names);
+        std::string name = channels[i];
+        std::string key = (i < keys.size()) ? keys[i] : "";
+        std::transform(name.begin(), name.end(), name.begin(), ::tolower);
+        processJoin(client, server, name, key);
     }
 }
