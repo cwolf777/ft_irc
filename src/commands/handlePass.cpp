50860398e7d019e1db80950c5523a5ae91534f22
Philipp Hofmann <phhofman@3-E-30.42heilbronn.de>
2026-01-29 14:07:05 +0100

    fixed handlePass

--- src/commands/handlePass.cpp
+++ src/commands/handlePass.cpp
@@ -3,26 +3,32 @@
 
 void handlePass(Client &client, Server &server, const IrcMsg &msg)
 {
-    if (client.getIsRegistered())
+    if (client.hasPass())
     {
         server.sendMsg(client, ":" + server.getServerName() + " 462 " + client.getNickname() + " :You may not reregister\r\n");
         return;
     }
-    if (msg.get_params().size() < 1)
+
+    if (msg.get_params().empty())
     {
-        server.sendMsg(client, ":" + server.getServerName() + "461 PASS :Not enough parameters\r\n");
+        server.sendMsg(client, ":" + server.getServerName() + " 461 * PASS :Not enough parameters\r\n");
         return;
     }
+
     std::string pass = msg.get_params()[0];
 
     if (pass != server.getPassword())
     {
-        server.sendMsg(client, ":" + server.getServerName() + "464 * :Password incorrect\r\n");
-        server.sendMsg(client, "ERROR :Closing Link: [Access denied by password]\r\n");
-        // throw ServerException("Auth failed"); /TODO:
+        // 464 ERR_PASSWDMISMATCH
+        server.sendMsg(client, ":" + server.getServerName() + " 464 * :Password incorrect\r\n");
+        server.sendMsg(client, "ERROR :Closing Link: Access denied (incorrect password)\r\n");
+        server.disconnectClient(client);
         return;
     }
+
     client.setHasPass(true);
     if (client.canRegister())
+    {
         server.sendWelcomeMessage(client);
+    }
 }
